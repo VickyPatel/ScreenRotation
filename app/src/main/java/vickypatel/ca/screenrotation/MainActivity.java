7252531f1ecb13bc4abac1cc719277c693cf6bc6
Vicky <vicky.7676@yahoo.in>
2016-01-09 22:56:19 -0500

    rotation that dont change anything

--- app/src/main/java/vickypatel/ca/screenrotation/MainActivity.java
+++ app/src/main/java/vickypatel/ca/screenrotation/MainActivity.java
@@ -8,11 +8,21 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
     int testVariable = 10;
+
+    EditText editText;
+    TextView textView;
+    Button button;
+    ArrayList<String> numbersArrayList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,12 +31,25 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        editText = (EditText) findViewById(R.id.editText);
+        textView = (TextView) findViewById(R.id.arrayTextView);
+        button = (Button) findViewById(R.id.addButton);
+
         Toast.makeText(this, "On create called", Toast.LENGTH_SHORT).show();
         Toast.makeText(this, "Test Variable " + testVariable, Toast.LENGTH_SHORT).show();
 
         //After some calculations
         testVariable = 20;
         Toast.makeText(this, "Test Variable " + testVariable, Toast.LENGTH_SHORT).show();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String number = editText.getText().toString();
+                numbersArrayList.add(number);
+                display();
+            }
+        });
 
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
@@ -58,5 +81,13 @@ public class MainActivity extends AppCompatActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void display(){
+        String temp = "";
+        for (int i=0; i<numbersArrayList.size(); i++){
+            temp = temp + numbersArrayList.get(i) + "\n";
+        }
+        textView.setText(temp);
     }
 }
